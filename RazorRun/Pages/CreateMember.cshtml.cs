9394e3afe2614a7e4e4289514b8e7bf4bb34498e
Thomas Stenholm <ths001@edu.zealand.dk>
2024-02-23 12:16:33 +0100

    T

---
+++ RazorRun/Pages/CreateMember.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorRun.Pages
+{
+    public class CreateMemberModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
