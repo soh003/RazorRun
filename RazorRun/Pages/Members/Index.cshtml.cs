651d9f6ad84300ffccc6418583968cb6b146c70f
Søren Vinther Hansen <soh003@edu.zealand.dk>
2024-02-23 13:11:03 +0100

    Mockdata added /model index.cshtml.cs added dependency injection,  Getall,

--- RazorRun/Pages/Members/Index.cshtml.cs
+++ RazorRun/Pages/Members/Index.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RunLib.model;
+using RunLib.repository;
 
 namespace RazorRun.Pages.Members
 {
     public class IndexModel : PageModel
+
     {
+        //Dependency injection
+        //Instans
+        private MemberRepository _repo;
+        //Konstruktør
+        public IndexModel(MemberRepository repo)
+        {
+            _repo = repo;
+        }
+
+        //Property til view
+        public List<Member> Members { get; set; }
+
         public void OnGet()
         {
+            Members = _repo.GetAll();
         }
     }
 }
--- RazorRun/Program.cs
+++ RazorRun/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddSingleton<MemberRepository>(new MemberRepository());
+builder.Services.AddSingleton<MemberRepository>(new MemberRepository(true));
 
 
 var app = builder.Build();
